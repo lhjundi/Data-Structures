7465d6331b4c6e41bf3b08c01913ed7a06b69583
Lucas Jundi Hikazudani <lhjundi@outlook.com>
2024-05-24 17:32:15 -0300

    Add main file

--- Sorting Algorithms/Quick Sort/Iterative Quick Sort/main.c
+++ Sorting Algorithms/Quick Sort/Iterative Quick Sort/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "quicksort.h"
 
-int main(){
-    printf("Iterative Quick Sort");
+int main() {
+    int numberOfElements = getNumberOfElements();
+    int *array = (int*) malloc(sizeof(int)*numberOfElements);
+    hasAllocationError(array);
+    fillArrayWithUserInput(array, numberOfElements);
+    quickSortIterative(array, 0, numberOfElements - 1);
+    showSortedArray(array, numberOfElements);
+    free(array);
+    return 0;
 }
