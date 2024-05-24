db014e2402fab537e2e566a0ffc5921b77126ed8
Lucas Jundi Hikazudani <lhjundi@outlook.com>
2024-05-24 17:01:28 -0300

    Removed unused operations of the Stack ADT to this usecase

--- Sorting Algorithms/Quick Sort/Iterative Quick Sort/dynamicStack.c
+++ Sorting Algorithms/Quick Sort/Iterative Quick Sort/dynamicStack.c
@@ -28,16 +28,6 @@ void initStack(STACK *pStack){
     pStack->top = NULL;
 }
 
-void showStack(STACK *pStack){
-    PTR address = pStack->top;
-    printf("Stack: \" ");
-    while (address != NULL){
-        printf("%d ", address->key);
-        address = address->next;
-    }
-    printf("\"\n");
-}
-
 // Returns the stack size
 // (number of elements)
 int size(STACK *pStack){
@@ -48,12 +38,6 @@ int size(STACK *pStack){
         address = address->next;
     }
     return size;
-}
-
-// Returns the size in bytes of the stack,
-// depends on the number of elements being used
-int sizeInBytes(STACK *pStack){
-    return(size(pStack) * sizeof(REGISTER)) + sizeof(STACK);
 }
 
 // Deletes the top element from the stack and copying
@@ -87,14 +71,4 @@ bool insertElemStack(REGISTER reg, STACK *pStack){
     new->next = pStack->top;
     pStack->top = new;
     return true;
-}
-/*
- returns the address of the top element of the stack and (if
- the stack is not empty) returns the key of that element in memory
- pointed to by the pointer key 
-*/
-PTR getTopElem(STACK *pStack, KEY_TYPE *key){
-    if(pStack->top != NULL) 
-        *key = pStack->top->key;
-    return pStack->top;
 }
